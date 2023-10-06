29cc137797dd891eeae974b7fdc6f8c58aa3e9bc
amenibelhadj <ameni.belhadj@esprit.tn>
2023-10-06 10:19:04 +0100

    CommitContrat

--- src/main/java/tn/esprit/spring/entities/Contrat.java
+++ src/main/java/tn/esprit/spring/entities/Contrat.java
@@ -1,91 +1,33 @@
 package tn.esprit.spring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Contrat implements Serializable {
-	
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long reference;
-	
+
 	@Temporal(TemporalType.DATE)
 	private Date dateDebut;
-	 
+
 	private String typeContrat;
- 
+
 	@JsonIgnore
-	@OneToOne(mappedBy="contrat")
+	@OneToOne(mappedBy = "contrat")
 	private Employe employe;
 
 	private float salaire;
 
-	public Contrat() {
-		super();
-	}
-	
-	public Contrat(Date dateDebut, String typeContrat, float salaire) {
-		this.dateDebut = dateDebut;
-		this.typeContrat = typeContrat;
-		this.salaire = salaire;
-	}
 
-
-	public Date getDateDebut() {
-		return dateDebut;
-	}
-
-	public void setDateDebut(Date dateDebut) {
-		this.dateDebut = dateDebut;
-	}
- 
-	public Long getReference() {
-		return reference;
-	}
-
-	public void setReference(Long reference) {
-		this.reference = reference;
-	}
-
-	public String getTypeContrat() {
-		return typeContrat;
-	}
-
-	public void setTypeContrat(String typeContrat) {
-		this.typeContrat = typeContrat;
-	}
-
-	public float getSalaire() {
-		return salaire;
-	}
-
-	public void setSalaire(float salaire) {
-		this.salaire = salaire;
-	}
-
-	public Employe getEmploye() {
-		return employe;
-	}
-
-	public void setEmploye(Employe employe) {
-		this.employe = employe;
-	}
- 
 }
